50e3269b180a54da97bbfadfcc13d903aa3de0f8
Jose Martinez <alias_one11@outlook.com>
2021-10-31 11:24:45 -0600

    2. completed

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import router from './router'
 import './assets/tailwind.css'
 // ⚫️⚫️☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰
 
-
-
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
